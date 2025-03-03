9e9e51058065b6e6c8d8edcbf3488dd0dbdd67f8
cart7982 <cart7982@vandals.uidaho.edu>
2025-03-03 14:54:14 -0800

    First upgrade to login system
    Login now creates a session.  Every page that comes after logging in should be .php and have session_start() somewhere near the top.

---
+++ CaravanSeraiCS360/session.php
@@ -0,0 +1,51 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "caravanserai";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+//Grab the username and password from login.
+$_Username = $_POST['username'];
+$_Password = $_POST['pwd'];
+
+echo $_Username;
+echo $_Password;
+
+//Check if that username and password combo exist in the database:
+$sql = "SELECT Username, Password FROM users WHERE Username='$_Username' AND Password='$_Password'";
+
+$result = $conn->query($sql);
+
+//Check if username/password combo exists in database.  
+//If so, start the session, otherwise go back to login.
+if($result->num_rows != 0){
+    session_start();
+    echo "Session started!";
+}
+else
+{
+    header('Location:login.html');
+}
+
+//Declare global session variables.
+//These variables can then be used in any session() page.
+$_SESSION["Username"] = $_Username;
+$_SESSION["Password"] = $_Password;
+
+$conn->close();
+
+//If a session started, go to product listings.
+if($result->num_rows!= 0){
+    header('Location:product_listings.php');
+}
+
+?>
--- CaravanSeraiCS360/signup.php
+++ CaravanSeraiCS360/signup.php
@@ -20,7 +20,7 @@ $_Username = $_POST['username'];
 $_Email = $_POST['email'];
 $_Password = $_POST['pwd'];
 
-//Grab the highest ID in the mem_ID column, then increment it by one for the new mem_ID to be assigned.
+//Grab the highest ID in the UserID column, then increment it by one for the new UserID to be assigned.
 $result = mysqli_query($conn, "SELECT MAX(UserID) AS max FROM users");
 $row = mysqli_fetch_array($result);
 $PrevID = $row['max'];
@@ -29,23 +29,23 @@ $NewID = intval($PrevID) + 1;
 if($_Username == NULL || $_Password == NULL || $_Email == NULL)
 {
     //If not all fields have been filled, return without committing.
-    header('Location:UserPage.php');
+    header('Location:signup.php');
 }
 else
 {
     //Check if user already exists
-    $sql = "SELECT Username, Password, Email FROM users WHERE Username='$_Username' AND Password='$_Password' AND Email='$_Email'";
+    $sql = "SELECT Username, Email FROM users WHERE Username='$_Username' AND Email='$_Email'";
 
     $result = $conn->query($sql);
 
     //If user exists, result will have >0 rows
     if($result->num_rows!= 0){
         echo "Registration failed!";
-        header('Location:index.php');
+        header('Location:signup.php');
     }
     else
     {
-        //Register the new user.
+        //Register the new user.  This assigns them a unique UserID.
         $sql = "INSERT INTO users (Username, Password, Email, UserID) VALUES ('$_Username', '$_Password', '$_Email', '$NewID')";
         //$sql = "INSERT INTO users (FirstName, LastName, Passwrd, Email) VALUES ('Saruman', 'TheWhite', 'mine', 'neutral@sauron.com')";
 
@@ -54,7 +54,7 @@ else
 
         $conn->close();
     }
-    header('Location:product_listings.html');
+    header('Location:login.html');
 }
 
 
