c4478704c0fed17583f4bab7fe4eeb8bc52eb5c0
nar-rise <bookwormer104@gmail.com>
2025-03-28 12:50:45 -0700

    first style update
    - Implemented dropdown navbar
    - Updated table formatting/colors
    - First round of styling front-end

--- OpenPlazaCS360Lab/product_description.php
+++ OpenPlazaCS360Lab/product_description.php
@@ -5,15 +5,32 @@
         <meta charset = "utf-8">
         <meta name = "viewport" content = "width = device-width, initial-scale = 1">
         <link href = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel = "stylesheet">
+        <link href = "style2.css" rel = "stylesheet">
 
         <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
-        <ul class="nav">
-            <li class = "nav-item">
-                <a class = "nav-link" href = "profile.php">User Profile</a>
-            </li>
-        </ul>
+    <div class = "navbar">    
+            <div class="dropdown" tabindex="1">
+                <i class="db2" tabindex="1"></i>
+                <a class="dropbtn">Account</a>
+                <div class="dropdown-content">
+                    <a href = "signup.html">Sign Up</a>
+                    <a href = "login.html">Log In</a>
+                    <a href = "logout.php">Log Out</a>
+                </div>
+            </div>
+
+            <div class="dropdown" tabindex="1">
+                <i class="db2" tabindex="1"></i>
+                <a class="dropbtn">Trade</a>
+                <div class="dropdown-content">
+                    <a href = "product_listings.php">Product Listings</a>
+                    <a href = "cart.php">Cart</a>
+                    <a href = "checkout.php">Checkout</a>
+                </div>
+            </div>
+        </div>
 
         <h1>PRODUCT DESCRIPTION</h1>
 
--- OpenPlazaCS360Lab/profile.php
+++ OpenPlazaCS360Lab/profile.php
@@ -5,6 +5,7 @@
         <meta charset = "utf-8">
         <meta name = "viewport" content = "width = device-width, initial-scale = 1">
         <link href = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel = "stylesheet">
+        <link href = "style2.css" rel = "stylesheet">
 
         <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     </head>
@@ -12,6 +13,28 @@
     <?php
     session_start();
     ?>
+        <div class = "navbar">    
+            <div class="dropdown" tabindex="1">
+                <i class="db2" tabindex="1"></i>
+                <a class="dropbtn">Account</a>
+                <div class="dropdown-content">
+                    <a href = "signup.html">Sign Up</a>
+                    <a href = "login.html">Log In</a>
+                    <a href = "logout.php">Log Out</a>
+                </div>
+            </div>
+
+            <div class="dropdown" tabindex="1">
+                <i class="db2" tabindex="1"></i>
+                <a class="dropbtn">Trade</a>
+                <div class="dropdown-content">
+                    <a href = "product_listings.php">Product Listings</a>
+                    <a href = "cart.php">Cart</a>
+                    <a href = "checkout.php">Checkout</a>
+                </div>
+            </div>
+        </div>
+        
         <h1>PROFILE</h1>
 
         <div class = "card">
