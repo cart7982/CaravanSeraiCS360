49ed1a7cc9cc363fc088a9b954dfad43e0fd814b
cart7982 <cart7982@vandals.uidaho.edu>
2025-03-31 11:14:35 -0700

    Minor updates

--- OpenPlazaCS360Lab/index.php
+++ OpenPlazaCS360Lab/index.php
@@ -35,7 +35,7 @@
                 Welcome to OpenPlaza!
             </div>
             <div class = "card-body">
-                Here you can post goods and make purchases from others, entirely anonymously!
+                The Grand Marketplace:
             </div>
         </div>
 
@@ -44,13 +44,18 @@
         $conn = mysqli_connect("localhost","root","","openplaza");
         $result = mysqli_query($conn,"SELECT * FROM products LIMIT 50");
         $data = $result->fetch_all(MYSQLI_ASSOC);
-        
+
         session_start();
         if(isset($_SESSION['UserID']))
         {
             $_UserID = $_SESSION["UserID"];
             header('Location:product_listings.php');
             exit();
+        }
+        else
+        {
+            session_unset();
+            session_destroy();
         }
         ?>
 
