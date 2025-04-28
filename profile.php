9300ac20f0ab73148cd287aa6417239c1f9146f5
nar-rise <bookwormer104@gmail.com>
2025-04-28 12:29:16 -0700

    remove junk
    get rid of junk modal

--- profile.php
+++ profile.php
@@ -799,24 +799,6 @@
                 </div>
             </div>
 
-            <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="confirmModalLabel">Confirm Action</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Would you like to send the product to another user in your group?
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                        <button type="button" class="btn btn-primary" id="confirmSubmitBtn">Yes, Accept Offer</button>
-                    </div>
-                    </div>
-                </div>
-            </div>
-
         </section>
 
         <?php 
@@ -830,5 +812,4 @@
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
-    
 </html>
